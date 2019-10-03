b0779fe5b08fdfc7d34abb5ff7b38d8407f24286
jsieving <31548155+jsieving@users.noreply.github.com>
2019-10-02 20:12:02 -0400

    fixing setup script

--- docker/d_setup_linux.sh
+++ docker/d_setup_linux.sh
@@ -3,4 +3,4 @@
 cp .dockin ~/.local/bin/
 cp d_setup_linux.sh ~/.local/bin/
 brc=~/.local/bin
-echo "source ${brc}/d_setup_linux.sh" >> ~/.bashrc
+echo "source ${brc}/d_scripts_linux.sh" >> ~/.bashrc
