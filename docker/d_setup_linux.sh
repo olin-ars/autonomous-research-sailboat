58f253c685fbf103864462028bcc0255532f39c6
jsieving <31548155+jsieving@users.noreply.github.com>
2019-10-02 20:18:26 -0400

    shashank doesn't know how to spell

--- docker/d_setup_linux.sh
+++ docker/d_setup_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cp .dockin ~/.local/bin/
-cp d_setup_linux.sh ~/.local/bin/
+cp d_scripts_linux.sh ~/.local/bin/
 brc=~/.local/bin
 echo "source ${brc}/d_scripts_linux.sh" >> ~/.bashrc
