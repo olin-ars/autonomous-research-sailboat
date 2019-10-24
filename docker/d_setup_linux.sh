6cb643dc8f4e5c7e683150cc8f2a8e20881eb838
Shashank Swaminathan <sh.swami235@gmail.com>
2019-10-23 21:34:10 -0400

    Added jq parser install

--- docker/d_setup_linux.sh
+++ docker/d_setup_linux.sh
@@ -4,3 +4,6 @@ cp .dockin ~/.local/bin/
 cp d_scripts_linux.sh ~/.local/bin/
 brc=~/.local/bin
 echo "source ${brc}/d_scripts_linux.sh" >> ~/.bashrc
+
+# Setup the json parsing bash script tool
+sudo apt-get install jq
