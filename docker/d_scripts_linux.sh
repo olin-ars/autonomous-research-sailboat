1f593cb56a9164b11b491e319e24fdf41d324df8
Elias Gabriel <egabriel@olin.edu>
2019-11-07 10:33:17 -0500

    Adjust base image

--- docker/d_scripts_linux.sh
+++ docker/d_scripts_linux.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 function useoars() {
-	  if [ -z $(docker ps -a --format "{{.Names}}" | grep "oars") ]; then
-				.dockin -s 1 -n oars -i olinoars/ros:2019-2020 -v oars-volume
-    else
-				.dockin -s 2 -n oars
-    fi
+	if [ -z $(docker ps -a --format "{{.Names}}" | grep "oars") ]; then
+        	.dockin -s 1 -n oars -i olinaquabots/base-setup:2019-2020 -v "/home/oliner/oars-research"
+	else
+		.dockin -s 2 -n oars
+	fi
 }
 
 function cleanoars_soft() {
-		docker rm oars
+	docker rm oars
 }
 
 function cleanoars_hard() {
-		docker rm oars
-		docker rmi oars-volume
+	docker rm oars
+	docker rmi oars-research
 }
 
 function resetoars_soft() {
-		docker rm oars
-		useoars
+	docker rm oars
+	useoars
 }
 
 function resetoars_hard() {
-		docker rm oars
-		docker rmi oars-volume
-		useoars
+	docker rm oars
+	docker rmi oars-research
+	useoars
 }
 
 function joinoars() {
-		docker exec -it oars bash
+	docker exec -it oars bash
 }
